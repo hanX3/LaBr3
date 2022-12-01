392e8810b9c7ac09961ac78f631065834f4b4005
hanX3 <hanxingchi@gmail.com>
2022-12-01 19:31:23 +0800

    DT5730_DPP_PSD ana

---
+++ ana_DT5720_DPP_PSD/main_draw.cpp
@@ -0,0 +1,41 @@
+#include "WaveAnalysis.h"
+
+#include <iostream>
+#include <fstream>
+#include <assert.h>
+#include <stdlib.h>
+
+#include "TString.h"
+#include "TApplication.h"
+
+#define DEBUGDRAWOPTION
+
+//
+int main(int argc, char *argv[])
+{
+  if(argc != 3){
+    std::cout << "need parameter" << std::endl;
+    std::cout << "like: ana 12 0" << std::endl;
+    std::cout << "means analysi file 39 entry 0 " << std::endl;
+    return -1;
+  }
+
+  TApplication ac("app", &argc, argv);
+
+  int run = atoi(argv[1]);
+  TString file_in = TString::Format("../raw2root_DT5720_DPP_PSD/run%04d.root", run);
+  std::cout << "analysis " << file_in << std::endl;
+  
+  int entry = atoi(argv[2]);
+  TString file_out = TString::Format("./data_R%04d_draw.root", run);
+  std::cout << "create " << file_out << std::endl;
+
+  WaveAnalysis *wa = new WaveAnalysis(file_in.Data(), file_out.Data(), 0, 0);
+  
+  wa->DrawEntry((Long64_t)entry);
+  // wa->ProcessEntry((Long64_t)entry);
+
+  ac.Run();
+
+  return 0;
+}
---
+++ ana_DT5720_DPP_PSD/set.h
@@ -0,0 +1,15 @@
+#define DEBUGDRAWOPTION
+
+#define MAXLENGTH 32768
+#define POLARITY -1
+
+// Baseline
+#define PRETRIGGER    30
+#define BASELINESTDCHECK 10.
+
+// MA Parameters
+#define MALENGTH    80
+
+// waveform Parameters
+#define DECAYTIMEPMT    7.5 // points
+#define DECAYTIMELABR3  5.0 // points
