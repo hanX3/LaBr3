7f08ebda07a563fa212c86491cc4fd44decff730
hanX3 <hanxingchi@gmail.com>
2022-12-14 08:52:05 +0800

    raw2root

---
+++ DT5730_DPP_PSD/raw2root/set.h
@@ -0,0 +1,6 @@
+
+#define FILE_PATH "../../../data/DT5730_DPP_PSD/DAQ" // no "/"
+#define FILE_HEAD "UNFILTERED/Data_run_" // no "/"
+// #define FILE_HEAD "UNFILTERED/Data_CH0@DT5730_1258_run_" // no "/"
+
+#define MAX_CHANNELS 8
